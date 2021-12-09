fafc1cf662f56808164d9ff0743ce66c21d6b78a
metekog <metekog@gmail.com>
2021-12-10 00:39:09 +0300

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>Met.</Logo>
+        <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Desc>
+        <SocialContainer>
+          <SocialIcon color="3b5999">
+            <FacebookIcon />
+          </SocialIcon>
+          <SocialIcon color="e4405f">
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color="55acee">
+            <TwitterIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Terms</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Products</ListItem>
+          <ListItem>Privacy</ListItem>
+          <ListItem>Contact</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </ContactItem>
+        <ContactItem>+99 555 45 45</ContactItem>
+        <ContactItem>contact@mail.com</ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"></Payment>
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
